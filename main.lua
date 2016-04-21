8095b57bb3e02ffc601cdf3cc0f9df7c0ed77b0c
Steven Roberts <sroberts@fenderq.com>
2016-04-21 16:22:27 -0700

    comments

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
+-- This function is called exactly once at the beginning of the game.
 function love.load()
 end
 
+-- Callback function used to draw on the screen every frame.
 function love.draw()
 	love.graphics.clear(0, 0, 255)
 	love.graphics.print("Press space to calc damage!", 64, 64)
 end
 
+-- Callback function triggered when a key is pressed.
 function love.keypressed(key, scancode, isrepeat)
 	if key == 'escape' then
 		love.event.quit()
@@ -15,6 +18,7 @@ function love.keypressed(key, scancode, isrepeat)
 	end
 end
 
+-- This gets called when we press the space bar key.
 function doit()
 	print("doit")
 end
