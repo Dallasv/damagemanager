74c642e8c5d89838b2143e95eb87eb8e97fc2bd1
Steven Roberts <sroberts@fenderq.com>
2016-04-21 16:43:02 -0700

    fancy command text input

--- main.lua
+++ main.lua
@@ -1,24 +1,44 @@
+local utf8 = require("utf8")
+
 -- This function is called exactly once at the beginning of the game.
 function love.load()
+	cmd = ""
 end
 
 -- Callback function used to draw on the screen every frame.
 function love.draw()
 	love.graphics.clear(0, 0, 255)
 	love.graphics.print("Press space to calc damage!", 64, 64)
+	love.graphics.printf(cmd, 64, 8, love.graphics.getWidth())
+end
+
+function love.textinput(t)
+	cmd = cmd .. t
 end
 
 -- Callback function triggered when a key is pressed.
 function love.keypressed(key, scancode, isrepeat)
+	if key == 'backspace' then
+		local byteoffset = utf8.offset(cmd, -1)
+		if byteoffset then
+			cmd = string.sub(cmd, 1, byteoffset - 1)
+		end
+	end
 	if key == 'escape' then
 		love.event.quit()
 	end
+	if key == 'return' then
+		doCommand()
+	end
 	if key == 'space' then
-		doit()
+		calcDamage()
 	end
 end
 
--- This gets called when we press the space bar key.
-function doit()
-	print("doit")
+function doCommand()
+	print(string.format("doCommand: %s", cmd))
+end
+
+function calcDamage()
+	print("calcDamage function called")
 end
