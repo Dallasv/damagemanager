ae0dbd10e70ce4c5b6a55f929e3d04b25db24aa9
Steven Roberts <sroberts@fenderq.com>
2016-04-21 16:11:21 -0700

    more stuff

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+	t.window.title = "DalVan - Damage Manager v1.0"
+	t.window.width = 640
+	t.window.height = 480
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,20 @@
+function love.load()
+end
+
 function love.draw()
-	love.graphics.print("Hello World!", 400, 300)
+	love.graphics.clear(0, 0, 255)
+	love.graphics.print("Press space to calc damage!", 64, 64)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+	if key == 'escape' then
+		love.event.quit()
+	end
+	if key == 'space' then
+		doit()
+	end
+end
+
+function doit()
+	print("doit")
 end
